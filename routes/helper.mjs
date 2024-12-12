132411bc379b9b3f8a6d6bf685657cc3ee6d5cc9
amaeljampen <amael.jampen@eduvaud.ch>
2024-12-12 13:43:07 +0100

    feat: Ajout des get, des fonctions CRUD et d'une liste de produits

---
+++ routes/helper.mjs
@@ -0,0 +1,39 @@
+import {products} from "../db/mock-product.mjs";
+
+const success = (message, data) => {
+    return {
+        message: message,
+        data: data,
+    };
+};
+
+const getUniqueId = (products) => {
+
+// On construit un tableau d'id de produits
+    const productsIds = products.map((product) => product.id);
+
+// La fonction passée à reduce compare deux éléments à la fois (a et b) et
+// retourne le plus grand des deux grâce à Math.max.
+// Au final, reduce parcourt tout le tableau productsIds, compare chaque ID
+// avec l'ID maximal trouvé jusqu'à présent, et retourne l'ID le plus élevé,
+// qui est stocké dans la variable maxId.
+    const maxId = productsIds.reduce((a, b) => Math.max(a, b));
+
+    return maxId + 1;
+};
+
+const getProduct = (productId) => {
+    return products.find((product) => product.id == productId);
+};
+
+const updateProduct = (productId, updatedProduct) => {
+        products[productId] = updatedProduct
+};
+
+const removeProduct = (productId) => {
+    products = products.filter((product) => product.id != productId);
+};
+
+
+
+export { getProduct, removeProduct, updateProduct, getUniqueId, success };
--- src/app.mjs
+++ src/app.mjs
@@ -1,11 +1,22 @@
 import express from "express";
 
 const app = express();
+
+app.use(express.json());
+
 const port = 3000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
+
+app.get("/api/", (req, res) => {
+    res.redirect(`http://localhost:${port}/`);
+});
+
+import { productsRouter } from "../routes/products.mjs";
+app.use("/api/products", productsRouter);
+
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
 });
